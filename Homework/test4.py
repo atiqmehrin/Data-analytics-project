1d0312c659e4979db995384564e4447c40858ca5
atiqmehrin <your-email@example.com>
2026-04-26 17:33:42 +0200

    file is moved to Homework directory

---
+++ Homework/test4.py
@@ -0,0 +1,11 @@
+# Sample DataFrames
+import pandas as pd
+#copy from main
+import numpy as np
+df_1 = pd.DataFrame({'ID': [1, 2, 3], 'Name': ['Alice', 'Bob', 'Charlie']})
+df_2 = pd.DataFrame({'ID': [2, 3, 4], 'Department': ['HR', 'IT', 'Finance']})
+
+# Outer Join
+df_outer = pd.merge(df_1, df_2, on='ID', how='outer'
+                    )
+print(df_outer)
